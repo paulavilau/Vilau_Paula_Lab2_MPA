81786001a9b785d9972b7006ac5dab24518a545c
Paula Vilau <vilaupaula@yahoo.ro>
2023-10-23 19:02:27 +0300

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vilau_Paula_Lab2.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public int? AuthorId { get; set; }
+        public decimal Price { get; set; }
+        public Author? Author { get; set; }
+        public ICollection<Order>? Orders { get; set; }
+    }
+}
